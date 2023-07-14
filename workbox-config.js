e217ecd0d82e0623676559b0b791486ef58ba846
eliasn <nebiyuelias1@gmail.com>
2023-07-15 04:10:06 +0800

    Fix issues

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,34 @@
 module.exports = {
   globDirectory: 'dist/apps/my-app',
   globPatterns: ['**/*.{txt,ico,html,js}'],
-  swDest: 'dist/apps/my-app/service-worker.js'
+  swDest: 'dist/apps/my-app/service-worker.js',
+  clientsClaim: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|gif|svg|woff|woff2)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        },
+      },
+    },
+  ],
+  offlineGoogleAnalytics: true,
+  cleanupOutdatedCaches: true,
+  navigateFallback: '/offline', // Replace with the correct URL of your offline page route
+  navigateFallbackDenylist: [
+    new RegExp('^/_'),
+    new RegExp('/[^/?]+\\.[^/]+$'),
+  ],
+  // Customize the following to match your project structure and offline page requirements
+  additionalManifestEntries: [
+    {
+      url: '/offline', // Replace with the correct URL of your offline page route
+      revision: null,
+    },
+  ],
 };
